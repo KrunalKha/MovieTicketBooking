92d294c210acb8dea1dd34f43627749ac5c994a0
Krunal Gangadhar Kharat <krunalkha@cybage.com>
2024-01-03 11:21:19 +0530

    Added Filter to Movies and Webseries

--- JavaScript/controller/filter_webSeries_details_controller.js
+++ JavaScript/controller/filter_webSeries_details_controller.js
@@ -2,28 +2,53 @@ import MovieService from "../services/movieService.js";
 import WebSeriesService from "../services/webSeriesService.js";
 
 
-$(document).ready(function(){
-    $(document).on('click','#filter-btn',function(){
+$(document).ready(function () {
+    $(document).on('click', '#filter-btn', function () {
 
-        //gettting value from filter
-        // const year = '2009';//hardcore value
+        let year_inp = $('input[name="year"]:checked').val();
+        let genre_inp = $('input[name="genre"]:checked').val();
 
-        //storing parameter value
-       const params = new URLSearchParams([['Year', '2008'],['Year','2007']]);
 
-       WebSeriesService.getWebSeriesDetailsbyFilter(params).then((response)=>{
-           
-         
-        let webSeries = response.data;
-        $(".content").html("<div class='webSeries-container'></div>");
-        $(".webSeries-container").append("<div class = 'card-container'></div>");
-        let card_count=1;
-         for(let series of webSeries){
-           
-            let card = `<div class = 'card card_${card_count}'></div>`;
-            let row = `
+
+        WebSeriesService.getWebSeriesDetails().then((response) => {
+
+
+            let webSeries = response.data;
+            //filter code goes here
+            function filterwebSeriesByYear(year) {
+                webSeries = webSeries.filter(series => series.Year < year);
+
+            }
+
+            function filterwebSeriesonYear(year) {
+                webSeries = webSeries.filter(series => series.Year == year);
+            }
+
+            if (year_inp == 'old') {
+                year_inp = 2019;
+                filterwebSeriesByYear(year_inp);
+            } else if (year_inp != 'all' && year_inp != "" && year_inp != undefined) {
+                filterwebSeriesonYear(year_inp);
+            } else {
+                console.log("error because of invalid value");
+            }
+
+
+
+            //filtering genre of movies
+            if (genre_inp != null && genre_inp != undefined && genre_inp != "") {
+                webSeries = webSeries.filter(series => series.Genre.includes(genre_inp));
+            }
+
+            $(".content").html("<div class='webSeries-container'></div>");
+            $(".webSeries-container").append("<div class = 'card-container'></div>");
+            let card_count = 1;
+            for (let series of webSeries) {
+
+                let card = `<div class = 'card card_${card_count}'></div>`;
+                let row = `
                 <div class ="card-image">
-                <img src="${series.Image.replace('C:\\fakepath\\','../images/webSeriesPosters/')}" alt="movie poster" >
+                <img src="${series.Image.replace('C:\\fakepath\\', '../images/webSeriesPosters/')}" alt="movie poster" >
 
                 </div>
                 <div class="overlay">
@@ -33,26 +58,26 @@ $(document).ready(function(){
 						<button style="margin-top: 10%;" class="book-now-btn" movieId='${series.id}'>Watch Now</button>
 					</div>
 				</div>
-            `  
-            $(".card-container").append(card);
-            // let bookBtn = `<button style="margin-top: 10%;" class="book-now-btn" movieId='${movie.id}'>Book Now</button>`
-          
-            $(".card_"+card_count).append(row);
+            `
+                $(".card-container").append(card);
+                // let bookBtn = `<button style="margin-top: 10%;" class="book-now-btn" movieId='${movie.id}'>Book Now</button>`
 
-           
-            // $(".card_"+card_count).append(bookBtn);
-            card_count=card_count+1;
-            
-         }
-        
- 
-        
-            
-        
-    }).catch((error)=>{
-        console.log(error);
+                $(".card_" + card_count).append(row);
+
+
+                // $(".card_"+card_count).append(bookBtn);
+                card_count = card_count + 1;
+
+            }
+
+
+
+
+
+        }).catch((error) => {
+            console.log(error);
+        })
     })
-})
 
 })
 
