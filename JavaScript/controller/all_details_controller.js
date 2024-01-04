880d5f2f14d47087df01a92b4694808d3751dbe1
Krunal Gangadhar Kharat <krunalkha@cybage.com>
2024-01-04 19:36:41 +0530

    updated city,added events to home, solved bugs

--- JavaScript/controller/all_details_controller.js
+++ JavaScript/controller/all_details_controller.js
@@ -2,7 +2,7 @@ import MovieService from "../services/movieService.js";
 import WebSeriesService from "../services/webSeriesService.js";
 import SportService from "../services/sportsService.js";
 import UserService from "../services/userService.js";
-import User from "../model/Users.js";
+import EventService from "../services/eventsService.js";
 
 $(document).ready(function () {
     let card_count = 1;
@@ -17,9 +17,9 @@ $(document).ready(function () {
             $(".content-header").append("<h2 class='content-title'>Popular Movies</h2>");
             $(".content-header").append("<button class='seeMore' id='moviesSeemore-btn'>see more...</button>");
             $(".movies-container").append("<div class = 'movies-card-container card-container'></div>");
-
+            
             for (let movie of movies) {
-
+               
                 let card = `<div class = 'card card_${card_count}'></div>`;
                 let row = `
                 <div class ="card-image">
@@ -48,6 +48,7 @@ $(document).ready(function () {
 
                 var categoryId = 'Films';
                 var Id = $(this).attr('movieId');
+                alert(Id);
                 window.location.href = 'booking_show.html?category=' + categoryId + '&id=' + Id;
 
             });
@@ -139,7 +140,7 @@ $(document).ready(function () {
 					<div class="card-info">
 						<h2 id="movie-title">"${sport.match}"</h2>
 						<p>Time: "${sport.time}"</p>
-						<button style="margin-top: 10%;" class="book-now-btn" sportId='${sport.id}'>Watch Now</button>
+						<button style="margin-top: 10%;" class="book-now-btn book-now-sport" sportId='${sport.id}'>Watch Now</button>
 					</div>
 				</div>
 
@@ -150,8 +151,7 @@ $(document).ready(function () {
                 card_count = card_count + 1;
             }
 
-            $('.book-now-btn').click(function () {
-
+            $('.book-now-sport').click(function () {
                 var categoryId = 'Sport';
                 var Id = $(this).attr('sportId');
                 window.location.href = 'booking_show.html?category=' + categoryId + '&id=' + Id;
@@ -170,6 +170,53 @@ $(document).ready(function () {
         })
 
     //add events here
+    EventService.getEventDetails()
+    .then((response)=>{
+        let events = response.data;
+       
+        $(".content").append("<div class='events-container'></div>");
+        $(".events-container").append("<div class = 'content-header3 header_cards'></div>");
+        $(".content-header3").append("<h2 class='content-title'>Upcoming Events</h2>");
+        $(".content-header3").append("<button class='seeMore' id='eventsSeemore-btn'>see more...</button>");
+        $(".events-container").append("<div class = 'events-card-container card-container'></div>");
+       
+       
+         for(let event of events){
+           
+            let card = `<div class = 'card card_${card_count}'></div>`;
+            let row = `
+                <div class ="card-image">
+                <img src="${event.Image.replace('C:\\fakepath\\','../images/eventPoster/')}" alt="event poster">
+                </div>
+                <div class="overlay">
+					<div class="card-info">
+						<h2 id="movie-title">"${event.title}"</h2>
+						<p>Artist Name: "${event.artist}"</p>
+						<button style="margin-top: 10%;" class="book-now-btn" eventId='${event.id}'>Book Now</button>
+					</div>
+				</div>
+
+            `  
+            $(".events-card-container").append(card);
+          
+            $(".card_"+card_count).append(row);
+            card_count=card_count+1;
+         }
+         $('.book-now-btn').click(function() {
+            
+            var categoryId = 'Event';
+            var Id = $(this).attr('eventId'); 
+            window.location.href = 'booking_show.html?category=' + categoryId + '&id=' + Id;
+            
+          });
+          $('#eventsSeemore-btn').click(function () {
+            window.location.href = 'events.html';
+
+        });
+        
+    }).catch((error)=>{
+        console.log(error);
+    });
 
 
     //making banner clickable
